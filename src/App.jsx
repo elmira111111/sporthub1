7e9ae1e7ec29f02a8c16abb036be8689931b051f
elmira <esenalievaelmira011@gmail.com>
2025-04-27 11:21:55 +0600

    admin часть бутурдум озумкун

--- src/App.jsx
+++ src/App.jsx
@@ -13,33 +13,34 @@ import LayoutAdmin from "./adminProfile/LayoutAdmin.jsx";
 import Post from './post/Post.jsx';
 import Trainers from './trainers/Trainers.jsx'
 import Inbox from './menu/inbox/Inbox.jsx';
-
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Layout />}>
+                <Route path="register" element={<RegisterPage />} />
+                <Route path="login" element={<LoginPage />} />
+                <Route path="forgot" element={<ForgotPass />} />
+                <Route path="pincode" element={<PinCode />} />
+                <Route path="imregister" element={<ImportReg />} />
+
+                <Route path="/" element={<Layout />}>
                     <Route index element={<GymHero />} />
-                    <Route path="register" element={<RegisterPage />} />
-                    <Route path="login" element={<LoginPage />} />
-                    <Route path="forgot" element={<ForgotPass />} />
-                    <Route path="pincode" element={<PinCode />} />
-                    <Route path="imregister" element={<ImportReg />} />
                     <Route path="reklama" element={<Reklama />} />
                     <Route path="basketbol" element={<Basketbol />} />
                     <Route path="personal" element={<PersonalInfo />} />
                 </Route>
 
                 <Route path="/admin" element={<LayoutAdmin />}>
-                    <Route path="profile" element={<PersonalInfo/>} />
+                    <Route path="profile" element={<PersonalInfo />} />
                     <Route path="trenery" element={<Trainers />} />
                     <Route path="inbox" element={<Inbox />} />
-                    <Route path="post" element={<Post/>} />
+                    <Route path="post" element={<Post />} />
                 </Route>
             </Routes>
         </BrowserRouter>
     );
 }
+
 
 export default App;
 
